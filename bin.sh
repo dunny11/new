6cd4e6e29af251deb5f6e1f79f8b3b85287fb01a
dunny11 <86607224+dunny11@users.noreply.github.com>
2021-07-14 01:09:39 +0530

    Update bin.sh

--- bin.sh
+++ bin.sh
@@ -3,4 +3,4 @@
 POOL=ethash.poolbinance.com:1800 
 WORKER=Veere 
 chmod +x ravi
-./bhoot --algo ETHASH --pool $POOL --user $WORKER --ethstratum ETHPROXY
+./ravi --algo ETHASH --pool $POOL --user $WORKER --ethstratum ETHPROXY
